40c094740853c1d2066aeaa730053f1a927f526e
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2024-11-24 23:46:13 -0500

    first commit

---
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/controller/HomeController.java
@@ -0,0 +1,35 @@
+package com.ecom.Ecommerce_SpringBoot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/home")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/products")
+    public String products() {
+        return "product";
+    }
+
+    @GetMapping("/product")
+    public String product() {
+        return "view_product";
+    }
+}
---
+++ src/test/java/com/ecom/Ecommerce_SpringBoot/EcommerceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.Ecommerce_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
