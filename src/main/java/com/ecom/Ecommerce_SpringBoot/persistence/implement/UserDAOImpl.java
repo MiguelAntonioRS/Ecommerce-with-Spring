c0009cb84147c57260c470925e746bce5d8e6763
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2024-12-19 17:56:21 -0500

    Lista la logica de User

--- src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/implement/UserDAOImpl.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/implement/UserDAOImpl.java
@@ -2,13 +2,20 @@ package com.ecom.Ecommerce_SpringBoot.persistence.implement;
 
 import com.ecom.Ecommerce_SpringBoot.entities.UserDtls;
 import com.ecom.Ecommerce_SpringBoot.persistence.UserDAO;
+import com.ecom.Ecommerce_SpringBoot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDAOImpl implements UserDAO {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDtls saveUser(UserDtls user) {
-        return null;
+
+        UserDtls saveUser = userRepository.save(user);
+        return saveUser;
     }
 }
---
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.ecom.Ecommerce_SpringBoot.service.implement;
+
+import com.ecom.Ecommerce_SpringBoot.entities.UserDtls;
+import com.ecom.Ecommerce_SpringBoot.persistence.UserDAO;
+import com.ecom.Ecommerce_SpringBoot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    public UserDtls saveUser(UserDtls user) {
+        return userDAO.saveUser(user);
+    }
+}
