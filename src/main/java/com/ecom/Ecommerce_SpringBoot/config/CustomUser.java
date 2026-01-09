8d79d141900725420ba86f391ecb2e8281352d88
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-09 13:14:44 -0500

    Finishing the configuration in the CustomUser.java

--- src/main/java/com/ecom/Ecommerce_SpringBoot/config/CustomUser.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/config/CustomUser.java
@@ -1,27 +1,56 @@
 package com.ecom.Ecommerce_SpringBoot.config;
 
 import com.ecom.Ecommerce_SpringBoot.entities.UserDtls;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Arrays;
 import java.util.Collection;
 
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomUser implements UserDetails {
 
     private UserDtls user;
 
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+        return Arrays.asList(authority);
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
