93fe4e519c4eafef8e406e2e79e3441f74c62520
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-09 13:07:00 -0500

    Creating new methods

--- src/main/java/com/ecom/Ecommerce_SpringBoot/config/CustomUser.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/config/CustomUser.java
@@ -1,4 +1,27 @@
 package com.ecom.Ecommerce_SpringBoot.config;
 
-public class CustomUser {
+import com.ecom.Ecommerce_SpringBoot.entities.UserDtls;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUser implements UserDetails {
+
+    private UserDtls user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
 }
