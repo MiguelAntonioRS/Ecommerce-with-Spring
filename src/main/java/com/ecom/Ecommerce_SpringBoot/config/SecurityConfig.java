5f86ca3d485e1c3213b4c79542fec67493995cdb
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-09 15:02:23 -0500

    Configuring the Security methods

--- src/main/java/com/ecom/Ecommerce_SpringBoot/config/SecurityConfig.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/config/SecurityConfig.java
@@ -1,4 +1,38 @@
 package com.ecom.Ecommerce_SpringBoot.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    public UserDetailsService userDetailsService() {
+
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+
+    public SecurityFilterChain securityFilterChain() {
+
+    }
 }
