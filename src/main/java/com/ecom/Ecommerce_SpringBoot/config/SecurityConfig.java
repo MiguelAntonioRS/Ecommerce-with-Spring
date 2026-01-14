56a564d21d60856f0f2c98c1f35a71c83b3d7d93
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-14 16:06:59 -0500

    Modifying the SecurityConfig class

--- src/main/java/com/ecom/Ecommerce_SpringBoot/config/SecurityConfig.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/config/SecurityConfig.java
@@ -3,18 +3,25 @@ package com.ecom.Ecommerce_SpringBoot.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class SecurityConfig {
 
     @Autowired
-    private AuthSuccessHandlerImpl authSuccessHandler;
+    private AuthenticationSuccessHandler authSuccessHandler;
+
+    @Autowired
+    @Lazy
+    private AuthFailureHandlerImpl authenticationFailureHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -48,6 +55,7 @@ public class SecurityConfig {
                 .formLogin(form -> form.loginPage("/signin")
                         .loginProcessingUrl("/login")
 //                        .defaultSuccessUrl("/")
+                        .failureHandler(authenticationFailureHandler)
                         .successHandler(authSuccessHandler))
                 .logout(logout -> logout.permitAll());
 
