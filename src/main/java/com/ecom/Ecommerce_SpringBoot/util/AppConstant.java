64bd7f8eb5503975abbfdcafbebbba794cadb6cd
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-14 15:21:16 -0500

    Creating a new constant

--- src/main/java/com/ecom/Ecommerce_SpringBoot/util/AppConstant.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/util/AppConstant.java
@@ -3,4 +3,6 @@ package com.ecom.Ecommerce_SpringBoot.util;
 public class AppConstant {
 
     public static final long UNLOCK_DURATION_TIME = 1 * 60 * 60 * 1000;
+
+    public static final long ATTEMPT_TIME = 3;
 }
