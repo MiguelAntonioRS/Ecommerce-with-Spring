fd66ae11177cde74ab6cd601e6e27f59f1a7307e
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-14 22:00:14 -0500

    Changing Values

--- src/main/java/com/ecom/Ecommerce_SpringBoot/util/AppConstant.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/util/AppConstant.java
@@ -2,7 +2,7 @@ package com.ecom.Ecommerce_SpringBoot.util;
 
 public class AppConstant {
 
-    public static final long UNLOCK_DURATION_TIME = 1 * 60 * 60 * 1000;
+    public static final long UNLOCK_DURATION_TIME = 3000; //1 * 60 * 60 * 1000;
 
     public static final long ATTEMPT_TIME = 3;
 }
