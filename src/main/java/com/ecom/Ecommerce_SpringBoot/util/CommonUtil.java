371cbf5b423420300ddcf2b26e67973e2e93a66d
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-18 12:17:15 -0500

    Creating a new method in the class generateUrl

--- src/main/java/com/ecom/Ecommerce_SpringBoot/util/CommonUtil.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/util/CommonUtil.java
@@ -1,9 +1,16 @@
 package com.ecom.Ecommerce_SpringBoot.util;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public class CommonUtil {
 
     public static Boolean sendMail() {
 
         return false;
+    }
+
+    public static String generateUrl(HttpServletRequest request) {
+
+        return null;
     }
 }
