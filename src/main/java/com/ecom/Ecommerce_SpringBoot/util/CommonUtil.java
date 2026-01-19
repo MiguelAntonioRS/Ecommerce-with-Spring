4610efc08730eb2970e47f59d8a41da79be8be35
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-19 12:23:34 -0500

    Throwing exception

--- src/main/java/com/ecom/Ecommerce_SpringBoot/util/CommonUtil.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/util/CommonUtil.java
@@ -1,17 +1,20 @@
 package com.ecom.Ecommerce_SpringBoot.util;
 
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
+import java.io.UnsupportedEncodingException;
+
 public class CommonUtil {
 
     @Autowired
     private static JavaMailSender mailSender;
 
-    public static Boolean sendMail(String url, String recipientEmail) {
+    public static Boolean sendMail(String url, String recipientEmail) throws UnsupportedEncodingException, MessagingException {
 
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
