d6f0cb962da5332fc3413c7327ba654147f2e41f
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-21 12:50:13 -0500

    Cleaning code

--- src/main/java/com/ecom/Ecommerce_SpringBoot/controller/HomeController.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/controller/HomeController.java
@@ -175,10 +175,6 @@ public class HomeController {
 
         UserDtls userByToken = userService.getUserByToken(token);
 
-        if (userByToken==null) {
-            model.addAttribute("errorMsg", "Your link is invalid or expired");
-            return "error";
-        }
 
         return "reset_password";
     }
