63f1454c2faf1e0d99830891a82bf514509c835c
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-24 11:43:52 -0500

    It was a test

--- src/main/java/com/ecom/Ecommerce_SpringBoot/controller/HomeController.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/controller/HomeController.java
@@ -57,8 +57,6 @@ public class HomeController {
             String email = principal.getName();
             UserDtls userDtls = userService.getUserByEmail(email);
             model.addAttribute("user", userDtls);
-        } else {
-            model.addAttribute("user", null);
         }
 
         List<Category> categories = categoryService.getAllActiveCategory();
