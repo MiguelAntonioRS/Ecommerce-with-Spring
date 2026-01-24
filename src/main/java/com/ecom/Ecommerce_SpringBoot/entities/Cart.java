2524a9644323791e03de0ce731114cf67fb0961b
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-24 17:14:20 -0500

    Creating his attributes

--- src/main/java/com/ecom/Ecommerce_SpringBoot/entities/Cart.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/entities/Cart.java
@@ -1,4 +1,12 @@
 package com.ecom.Ecommerce_SpringBoot.entities;
 
 public class Cart {
+
+    private int id;
+
+    private UserDtls user;
+
+    private Product product;
+
+    private int quantity;
 }
