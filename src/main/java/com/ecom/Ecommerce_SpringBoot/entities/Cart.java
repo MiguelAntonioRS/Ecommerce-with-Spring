d1ee66173c39d283b6c5ab47072aaba5bfbbe8c1
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-24 17:20:00 -0500

    Noting the other attributes

--- src/main/java/com/ecom/Ecommerce_SpringBoot/entities/Cart.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/entities/Cart.java
@@ -1,9 +1,6 @@
 package com.ecom.Ecommerce_SpringBoot.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,8 +17,10 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @ManyToOne
     private UserDtls user;
 
+    @ManyToOne
     private Product product;
 
     private int quantity;
