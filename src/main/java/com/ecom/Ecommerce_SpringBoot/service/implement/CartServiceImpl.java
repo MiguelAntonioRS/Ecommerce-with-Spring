566a4c053546f5d1867048f18f8ed63527aa2d4a
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-25 12:39:26 -0500

    Applying the methods created above

--- src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/CartServiceImpl.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/CartServiceImpl.java
@@ -1,8 +1,26 @@
 package com.ecom.Ecommerce_SpringBoot.service.implement;
 
+import com.ecom.Ecommerce_SpringBoot.entities.Cart;
+import com.ecom.Ecommerce_SpringBoot.repository.CartRepository;
 import com.ecom.Ecommerce_SpringBoot.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CartServiceImpl implements CartService {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Override
+    public Cart cartSave(int productId, int userId) {
+        return null;
+    }
+
+    @Override
+    public List<Cart> getCartsByUser(int userId) {
+        return null;
+    }
 }
