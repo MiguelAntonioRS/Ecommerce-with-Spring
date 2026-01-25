1e4136e71e803734a7f04c9e1a4509ee6a09aa6c
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-25 12:37:24 -0500

    Creating methods in the interface

--- src/main/java/com/ecom/Ecommerce_SpringBoot/service/CartService.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/CartService.java
@@ -1,7 +1,13 @@
 package com.ecom.Ecommerce_SpringBoot.service;
 
+import com.ecom.Ecommerce_SpringBoot.entities.Cart;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public interface CartService {
+
+    public Cart cartSave(int productId, int userId);
+
+    public List<Cart> getCartsByUser(int userId);
 }
