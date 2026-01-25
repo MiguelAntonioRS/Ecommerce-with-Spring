9badb3a387fd5a870eec5a1ef4ba0805b7932491
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-25 12:33:53 -0500

    Extends JpaRepository

--- src/main/java/com/ecom/Ecommerce_SpringBoot/repository/CartRepository.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/repository/CartRepository.java
@@ -1,7 +1,9 @@
 package com.ecom.Ecommerce_SpringBoot.repository;
 
+import com.ecom.Ecommerce_SpringBoot.entities.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartRepository {
+public interface CartRepository extends JpaRepository<Cart, Integer> {
 }
