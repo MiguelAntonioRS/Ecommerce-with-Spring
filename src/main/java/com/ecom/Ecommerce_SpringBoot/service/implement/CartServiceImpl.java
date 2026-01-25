f173cd914269a203652539b878b12b40ee8b5e04
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-25 12:41:44 -0500

    Injecting Repository

--- src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/CartServiceImpl.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/CartServiceImpl.java
@@ -2,10 +2,11 @@ package com.ecom.Ecommerce_SpringBoot.service.implement;
 
 import com.ecom.Ecommerce_SpringBoot.entities.Cart;
 import com.ecom.Ecommerce_SpringBoot.repository.CartRepository;
+import com.ecom.Ecommerce_SpringBoot.repository.ProductRepository;
+import com.ecom.Ecommerce_SpringBoot.repository.UserRepository;
 import com.ecom.Ecommerce_SpringBoot.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
@@ -13,6 +14,12 @@ public class CartServiceImpl implements CartService {
 
     @Autowired
     private CartRepository cartRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @Override
     public Cart cartSave(int productId, int userId) {
