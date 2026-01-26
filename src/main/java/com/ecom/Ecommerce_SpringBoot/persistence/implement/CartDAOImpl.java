47a95301bafd86a6fd8316d0354a5c571822c756
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-25 21:14:57 -0500

    Implementing this methods in the CartDAOImpl

--- src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/implement/CartDAOImpl.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/implement/CartDAOImpl.java
@@ -1,8 +1,21 @@
 package com.ecom.Ecommerce_SpringBoot.persistence.implement;
 
+import com.ecom.Ecommerce_SpringBoot.entities.Cart;
 import com.ecom.Ecommerce_SpringBoot.persistence.CartDAO;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class CartDAOImpl implements CartDAO {
+
+    @Override
+    public Cart cartSave(int productId, int userId) {
+        return null;
+    }
+
+    @Override
+    public List<Cart> getCartsByUser(int userId) {
+        return null;
+    }
 }
