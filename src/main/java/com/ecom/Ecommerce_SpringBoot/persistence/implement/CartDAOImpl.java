71c7537bc1f09b87e3994afde9dfb85a55b12012
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-25 21:17:40 -0500

    Finishing the CartDAOImpl for now

--- src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/implement/CartDAOImpl.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/implement/CartDAOImpl.java
@@ -1,17 +1,52 @@
 package com.ecom.Ecommerce_SpringBoot.persistence.implement;
 
 import com.ecom.Ecommerce_SpringBoot.entities.Cart;
+import com.ecom.Ecommerce_SpringBoot.entities.Product;
+import com.ecom.Ecommerce_SpringBoot.entities.UserDtls;
 import com.ecom.Ecommerce_SpringBoot.persistence.CartDAO;
+import com.ecom.Ecommerce_SpringBoot.repository.CartRepository;
+import com.ecom.Ecommerce_SpringBoot.repository.ProductRepository;
+import com.ecom.Ecommerce_SpringBoot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 
 @Component
 public class CartDAOImpl implements CartDAO {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CartRepository cartRepository;
+
     @Override
     public Cart cartSave(int productId, int userId) {
-        return null;
+
+        UserDtls userDtls = userRepository.findById(userId).get();
+        Product product = productRepository.findById(productId).get();
+        Cart statusCart = cartRepository.findByUserIdAndProductId(productId, userId);
+        Cart cart = null;
+
+        if (ObjectUtils.isEmpty(statusCart)) {
+            cart = new Cart();
+            cart.setProduct(product);
+            cart.setUser(userDtls);
+            cart.setQuantity(1);
+            cart.setTotalPrice(1 * product.getDiscountPrice());
+        } else {
+            cart = statusCart;
+            cart.setQuantity(cart.getQuantity() + 1);
+            cart.setTotalPrice(cart.getQuantity() * cart.getProduct().getDiscountPrice());
+        }
+        Cart saveCart = cartRepository.save(cart);
+
+        return saveCart;
     }
 
     @Override
