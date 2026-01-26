0b76d4e6eb1f0c35c0cca4a6dd4fa502488b11a4
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-25 21:12:09 -0500

    Creating a new methods

--- src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/CartDAO.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/CartDAO.java
@@ -1,7 +1,14 @@
 package com.ecom.Ecommerce_SpringBoot.persistence;
 
+import com.ecom.Ecommerce_SpringBoot.entities.Cart;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public interface CartDAO {
+
+    public Cart cartSave(int productId, int userId);
+
+    public List<Cart> getCartsByUser(int userId);
 }
