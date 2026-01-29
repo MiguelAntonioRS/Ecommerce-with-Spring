ce2748f17a96990666edef5e4e471510355dc562
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-01-29 13:30:24 -0500

    Test for render

--- src/main/java/com/ecom/Ecommerce_SpringBoot/controller/UserController.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/controller/UserController.java
@@ -70,7 +70,7 @@ public class UserController {
         List<Cart> carts = cartService.getCartsByUser(userDtls.getId());
         model.addAttribute("carts", carts);
 
-        return "/cart";
+        return "user/cart";
     }
 
     private UserDtls getLoggedInUserDetails(Principal principal) {
