dd1da7278812a6aadf338b22cf35afbd97912eb9
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-05 17:25:15 -0500

    Noting the entity with lombok annotations

--- src/main/java/com/ecom/Ecommerce_SpringBoot/entities/ProductOrder.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/entities/ProductOrder.java
@@ -1,7 +1,17 @@
 package com.ecom.Ecommerce_SpringBoot.entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.util.Date;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class ProductOrder {
 
     private int id;
