e4c56c4c3fa06bc0fb2349d222913129a4e4b19b
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-05 17:23:06 -0500

    Creating news attributes

--- src/main/java/com/ecom/Ecommerce_SpringBoot/entities/ProductOrder.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/entities/ProductOrder.java
@@ -1,4 +1,22 @@
 package com.ecom.Ecommerce_SpringBoot.entities;
 
+import java.util.Date;
+
 public class ProductOrder {
+
+    private int id;
+
+    private String orderId;
+
+    private Date orderDate;
+
+    private Product product;
+
+    private double price;
+
+    private int quantity;
+
+    private UserDtls user;
+
+    private String status;
 }
