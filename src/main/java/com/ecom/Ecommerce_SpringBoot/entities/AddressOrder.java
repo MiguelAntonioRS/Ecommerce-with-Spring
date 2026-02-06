72df4f471e5d5b3568572b141357b6948b15c82c
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-05 19:20:20 -0500

    Creating some attributes

--- src/main/java/com/ecom/Ecommerce_SpringBoot/entities/AddressOrder.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/entities/AddressOrder.java
@@ -1,4 +1,14 @@
 package com.ecom.Ecommerce_SpringBoot.entities;
 
 public class AddressOrder {
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String mobile;
 }
