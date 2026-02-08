df7ce2925dcbe2f7b1f73c9b491e36ff640fb9e8
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-08 17:56:19 -0500

    Injecting the CartRepository in the OrderServiceImpl

--- src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/OrderServiceImpl.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ecom.Ecommerce_SpringBoot.service.implement;
 
 import com.ecom.Ecommerce_SpringBoot.entities.ProductOrder;
+import com.ecom.Ecommerce_SpringBoot.repository.CartRepository;
 import com.ecom.Ecommerce_SpringBoot.repository.OrderRepository;
 import com.ecom.Ecommerce_SpringBoot.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,9 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private CartRepository cartRepository;
 
     @Override
     public ProductOrder saveOrder(int userId) {
