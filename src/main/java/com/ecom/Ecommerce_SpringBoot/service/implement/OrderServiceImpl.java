bc4a5af24d3ea1716c5b82a35c104362356fdd20
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-08 17:28:41 -0500

    Injecting the OrderRepository in the OrderServiceImpl

--- src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/OrderServiceImpl.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/OrderServiceImpl.java
@@ -1,11 +1,16 @@
 package com.ecom.Ecommerce_SpringBoot.service.implement;
 
 import com.ecom.Ecommerce_SpringBoot.entities.ProductOrder;
+import com.ecom.Ecommerce_SpringBoot.repository.OrderRepository;
 import com.ecom.Ecommerce_SpringBoot.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
 
     @Override
     public ProductOrder saveOrder(int userId) {
