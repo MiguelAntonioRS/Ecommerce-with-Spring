ee8c803609e04ca4cb8fcbe252aa9c24f9ab3caa
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-08 10:49:14 -0500

    Configuring this class

--- src/main/java/com/ecom/Ecommerce_SpringBoot/repository/OrderRepository.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.ecom.Ecommerce_SpringBoot.repository;
 
+import com.ecom.Ecommerce_SpringBoot.entities.ProductOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository {
+public interface OrderRepository extends JpaRepository<ProductOrder, Integer> {
 }
