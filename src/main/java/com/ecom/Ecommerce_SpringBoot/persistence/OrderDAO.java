bc40330edc25158457ee7bdbb15245b416dd30a6
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-08 18:12:53 -0500

    Creating a new method in this interface class

--- src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/OrderDAO.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/OrderDAO.java
@@ -1,7 +1,10 @@
 package com.ecom.Ecommerce_SpringBoot.persistence;
 
+import com.ecom.Ecommerce_SpringBoot.entities.ProductOrder;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface OrderDAO {
+
+    public ProductOrder saveOrder(int userId);
 }
