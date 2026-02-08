5688c8f3ecc4bbfb151b71cd7aa52ef877bf3047
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-08 17:24:01 -0500

    Creating a new method

--- src/main/java/com/ecom/Ecommerce_SpringBoot/service/OrderService.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/OrderService.java
@@ -1,7 +1,10 @@
 package com.ecom.Ecommerce_SpringBoot.service;
 
+import com.ecom.Ecommerce_SpringBoot.entities.ProductOrder;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface OrderService {
+
+    public ProductOrder saveOrder(int userId);
 }
