459bf2afc9ab5789db291fb8dd4c76d5092937eb
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-08 17:24:58 -0500

    Implementing this new method

--- src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/OrderServiceImpl.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/OrderServiceImpl.java
@@ -1,8 +1,14 @@
 package com.ecom.Ecommerce_SpringBoot.service.implement;
 
+import com.ecom.Ecommerce_SpringBoot.entities.ProductOrder;
 import com.ecom.Ecommerce_SpringBoot.service.OrderService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+
+    @Override
+    public ProductOrder saveOrder(int userId) {
+        return null;
+    }
 }
