71bbe81f03949b43076fa69d540459277f12168b
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-09 12:06:24 -0500

    Implementing this new parameter in the method

--- src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/implement/OrderDaoImpl.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/implement/OrderDaoImpl.java
@@ -1,6 +1,7 @@
 package com.ecom.Ecommerce_SpringBoot.persistence.implement;
 
 import com.ecom.Ecommerce_SpringBoot.entities.ProductOrder;
+import com.ecom.Ecommerce_SpringBoot.entities.RequestOrder;
 import com.ecom.Ecommerce_SpringBoot.persistence.OrderDAO;
 import org.springframework.stereotype.Component;
 
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class OrderDaoImpl implements OrderDAO {
 
     @Override
-    public ProductOrder saveOrder(int userId) {
+    public ProductOrder saveOrder(int userId, RequestOrder requestOrder) {
         return null;
     }
 }
