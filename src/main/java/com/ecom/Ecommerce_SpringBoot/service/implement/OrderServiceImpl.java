c6bb282e0a2809c1eb683ad8c7d020ec614f0a34
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-09 11:53:32 -0500

    Creating a list

--- src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/OrderServiceImpl.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/OrderServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ecom.Ecommerce_SpringBoot.service.implement;
 
+import com.ecom.Ecommerce_SpringBoot.entities.Cart;
 import com.ecom.Ecommerce_SpringBoot.entities.ProductOrder;
 import com.ecom.Ecommerce_SpringBoot.repository.CartRepository;
 import com.ecom.Ecommerce_SpringBoot.repository.OrderRepository;
 import com.ecom.Ecommerce_SpringBoot.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -18,6 +20,9 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public ProductOrder saveOrder(int userId) {
+
+        List<Cart> cartList = cartRepository.findByUserId(userId);
+
         return null;
     }
 }
