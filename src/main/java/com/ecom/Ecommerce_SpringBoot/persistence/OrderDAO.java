47b1722d34922fb043e309e3999498d76d9432cc
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-09 12:06:03 -0500

    Passing a new parameter in the method

--- src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/OrderDAO.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/OrderDAO.java
@@ -1,10 +1,11 @@
 package com.ecom.Ecommerce_SpringBoot.persistence;
 
 import com.ecom.Ecommerce_SpringBoot.entities.ProductOrder;
+import com.ecom.Ecommerce_SpringBoot.entities.RequestOrder;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface OrderDAO {
 
-    public ProductOrder saveOrder(int userId);
+    public ProductOrder saveOrder(int userId, RequestOrder requestOrder);
 }
