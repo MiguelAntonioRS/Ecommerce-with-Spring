e7da50c2c28ed4f48ed8d7dd3d4d10e3cc96e288
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-09 15:33:24 -0500

    Creating a new method named saveOrder

--- src/main/java/com/ecom/Ecommerce_SpringBoot/controller/UserController.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/controller/UserController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
@@ -86,6 +83,11 @@ public class UserController {
 
     @GetMapping("/orders")
     public String pageOrder() {
+        return "user/order";
+    }
+
+    @PostMapping("/save-order")
+    public String saveOrder() {
         return "user/order";
     }
 
