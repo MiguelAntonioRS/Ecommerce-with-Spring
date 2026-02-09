012b8571407abd24650073122c517761ab5095f6
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-09 11:59:50 -0500

    Creating the first attributes

--- src/main/java/com/ecom/Ecommerce_SpringBoot/entities/RequestOrder.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/entities/RequestOrder.java
@@ -1,4 +1,22 @@
 package com.ecom.Ecommerce_SpringBoot.entities;
 
 public class RequestOrder {
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String mobile;
+
+    private String address;
+
+    private String city;
+
+    private String state;
+
+    private String pincode;
 }
