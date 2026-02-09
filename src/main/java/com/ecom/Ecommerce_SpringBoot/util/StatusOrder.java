8f5d97aefb01c3240ea8e86e63aa675371e6fa18
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-09 12:22:48 -0500

    Starting the enum class logic

--- src/main/java/com/ecom/Ecommerce_SpringBoot/util/StatusOrder.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/util/StatusOrder.java
@@ -1,4 +1,17 @@
 package com.ecom.Ecommerce_SpringBoot.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
 public enum StatusOrder {
+
+    IN_PROGRESS(1, "In Progress"), ORDER_RECEIVED(2, "Order Received"),
+    PRODUCT_PACKED(3, "Product Packed"), OUT_FOR_DELIVERY(4, "Out for Delivery"),
+    DELIVERED(5, "Delivered");
+
+    private int id;
+
+    private String name;
 }
