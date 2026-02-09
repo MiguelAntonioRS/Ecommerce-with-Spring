0099166d09404edcee8e4653fbd1404c6363c86f
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-09 12:12:09 -0500

    Starting the for logic

--- src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/OrderServiceImpl.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/OrderServiceImpl.java
@@ -8,7 +8,10 @@ import com.ecom.Ecommerce_SpringBoot.repository.OrderRepository;
 import com.ecom.Ecommerce_SpringBoot.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -26,7 +29,17 @@ public class OrderServiceImpl implements OrderService {
 
         for (Cart cart:cartList) {
 
+            ProductOrder productOrder = new ProductOrder();
 
+            productOrder.setOrderId(UUID.randomUUID().toString());
+            productOrder.setOrderDate(new Date());
+            productOrder.setProduct(cart.getProduct());
+            productOrder.setPrice(cart.getProduct().getDiscountPrice());
+
+            productOrder.setQuantity(cart.getQuantity());
+            productOrder.setUser(cart.getUser());
+
+            productOrder.setStatus("");
         }
 
         return null;
