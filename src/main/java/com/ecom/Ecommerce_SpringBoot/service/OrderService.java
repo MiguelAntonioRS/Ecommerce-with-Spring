d9205e6a854ccaba987088cbcbef9f9041890beb
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-09 12:05:59 -0500

    Passing a new parameter in the method

--- src/main/java/com/ecom/Ecommerce_SpringBoot/service/OrderService.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/OrderService.java
@@ -1,10 +1,11 @@
 package com.ecom.Ecommerce_SpringBoot.service;
 
 import com.ecom.Ecommerce_SpringBoot.entities.ProductOrder;
+import com.ecom.Ecommerce_SpringBoot.entities.RequestOrder;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface OrderService {
 
-    public ProductOrder saveOrder(int userId);
+    public ProductOrder saveOrder(int userId, RequestOrder requestOrder);
 }
