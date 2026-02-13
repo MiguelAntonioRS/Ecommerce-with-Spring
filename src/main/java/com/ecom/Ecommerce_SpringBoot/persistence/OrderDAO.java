112279985b523b4bf644fbc0d4c26bbcce5f8b17
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-13 12:06:14 -0500

    Creating a new DAO implementation method

--- src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/OrderDAO.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/persistence/OrderDAO.java
@@ -4,8 +4,12 @@ import com.ecom.Ecommerce_SpringBoot.entities.ProductOrder;
 import com.ecom.Ecommerce_SpringBoot.entities.RequestOrder;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface OrderDAO {
 
     public void saveOrder(int userId, RequestOrder requestOrder);
+
+    public List<ProductOrder> getOrdersByUser(int userId);
 }
