2f8e2e0e5276e7a1da53ba0a926aee0e527d675d
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-13 12:09:57 -0500

    Creating a new method in the OrderRepository

--- src/main/java/com/ecom/Ecommerce_SpringBoot/repository/OrderRepository.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/repository/OrderRepository.java
@@ -4,6 +4,10 @@ import com.ecom.Ecommerce_SpringBoot.entities.ProductOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<ProductOrder, Integer> {
+
+    List<ProductOrder> findByUserId(int userId);
 }
