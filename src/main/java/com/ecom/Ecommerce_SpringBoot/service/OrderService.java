34337f5976ad140860a56356fb12dc1ed8eeb2a0
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-13 12:04:12 -0500

    Creating a new implementation method

--- src/main/java/com/ecom/Ecommerce_SpringBoot/service/OrderService.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/OrderService.java
@@ -4,8 +4,12 @@ import com.ecom.Ecommerce_SpringBoot.entities.ProductOrder;
 import com.ecom.Ecommerce_SpringBoot.entities.RequestOrder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface OrderService {
 
     public void saveOrder(int userId, RequestOrder requestOrder);
+
+    public List<ProductOrder> getOrdersByUser(int userId);
 }
