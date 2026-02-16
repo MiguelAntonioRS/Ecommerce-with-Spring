ef74fbd535e1850bd1f0d793e53dcc7bea665111
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-16 11:28:23 -0500

    Returning the method findAll()

--- src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/OrderServiceImpl.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/OrderServiceImpl.java
@@ -86,6 +86,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<ProductOrder> getAllOrdersByUser() {
-        return null;
+
+        return orderRepository.findAll();
     }
 }
