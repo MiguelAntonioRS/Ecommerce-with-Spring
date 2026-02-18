fffb432da899a5b31e2534d16117e9b2e4359fb5
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-18 12:01:49 -0500

    fix(return) Doing the logic in the ProductServiceImpl

--- src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/ProductServiceImpl.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/service/implement/ProductServiceImpl.java
@@ -47,6 +47,6 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<Product> searchProduct(String search) {
-        productDAO.searchProduct(search);
+        return productDAO.searchProduct(search);
     }
 }
