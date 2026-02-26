7c0692ad7c77f27a57bc6d4596ded84efccc9039
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-02-26 12:27:13 -0500

    feat(Util): adding new content : the status order

--- src/main/java/com/ecom/Ecommerce_SpringBoot/util/CommonUtil.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/util/CommonUtil.java
@@ -57,6 +57,7 @@ public class CommonUtil {
                 + "<p>Price : [[price]]</p>"
                 + "<p>Payment Type : [[paymentType]]</p>";
 
+        content = content.replace("[[orderStatus]]", codeStatus);
         content = content.replace("[[productName]]", productOrder.getProduct().getTitle());
         content = content.replace("[[category]]", productOrder.getProduct().getCategory());
         content = content.replace("[[quantity]]", String.valueOf(productOrder.getQuantity()));
