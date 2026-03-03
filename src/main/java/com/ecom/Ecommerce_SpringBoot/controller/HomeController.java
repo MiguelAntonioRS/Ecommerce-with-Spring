2209b55cb7ab3d1eb61e1842b740be0078e39c09
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-03-03 11:42:40 -0500

    fix: HomeController for cloudinary

--- src/main/java/com/ecom/Ecommerce_SpringBoot/controller/HomeController.java
+++ src/main/java/com/ecom/Ecommerce_SpringBoot/controller/HomeController.java
@@ -47,6 +47,7 @@ public class HomeController {
     @ModelAttribute
     public void getUsersDetails(Principal principal, Model model) {
         if (principal != null) {
+
             String email = principal.getName();
             UserDtls user = userService.getUserByEmail(email);
             model.addAttribute("user", user);
